a0ec1880084b35c01def9bc5c00f7d5ea6e62fea
ravindu0823 <guestpc87@gmail.com>
2024-01-01 10:21:34 +0530

    Created migrations

---
+++ HospitalMgrSystem.DataAccess/Migrations/20231228055834_A66.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalMgrSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class A66 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userRole",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ServiceID",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "userRole",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ServiceID",
+                table: "Invoices",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
