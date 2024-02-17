46e584700d6f94f536c327302b6781cd51bc0a3d
ravindu0823 <guestpc87@gmail.com>
2024-02-17 07:43:09 +0530

    feat: added new objects

--- HospitalMgrSystemUI/Models/Report.cs
+++ HospitalMgrSystemUI/Models/Report.cs
@@ -7,7 +7,19 @@ namespace HospitalMgrSystemUI.Models
         public int paidStatus { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        public int InvoicedType { get; set; }
         public List<OPD>? listopd { get; set; }
+        public List<OPD>? listChanneling { get; set; }
+        public List<OPD>? listPartialPaidOPD { get; set; }
+        public List<OPD>? listPartialPaidChanneling { get; set; }
+        public List<OPD>? listNeedToPayOPD { get; set; }
+        public List<OPD>? listNeedToPayChanneling { get; set; }
+        public List<OPD>? listNightShiftOPD { get; set; }
+        public List<OPD>? listNightShiftChanneling { get; set; }
+        public List<OPD>? listNotPaidOPD { get; set; }
+        public List<OPD>? listNotPaidChanneling { get; set; }
+        public Payment? paymentData { get; set; }
+        public List<OPDDrugus>? listopdGrugs { get; set; }
 
     }
 }
