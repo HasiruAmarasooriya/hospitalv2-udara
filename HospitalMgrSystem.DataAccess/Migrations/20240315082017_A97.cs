9c769b8bd2a3c68ac2da0d13b241662854d8ac07
ravindu0823 <guestpc87@gmail.com>
2024-03-17 18:40:51 +0530

    Created migrations

---
+++ HospitalMgrSystem.DataAccess/Migrations/20240315082017_A97.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalMgrSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class A97 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SchedularId",
+                table: "OtherTransactions",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SchedularId",
+                table: "OtherTransactions");
+        }
+    }
+}
