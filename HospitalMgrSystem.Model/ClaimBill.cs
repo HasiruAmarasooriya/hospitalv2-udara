45471d6860f299a8396e62cdb8b360352906b1e0
ravindu0823 <guestpc87@gmail.com>
2024-04-03 11:30:04 +0530

    Created ClaimBill model

---
+++ HospitalMgrSystem.Model/ClaimBill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgrSystem.Model
+{
+    public class ClaimBill
+    {
+        public int Id { get; set; }
+        public int PatientID { get; set; }
+
+        [ForeignKey("PatientID")]
+        public Patient? Patient { get; set; }
+        public int ConsultantId { get; set; }
+
+        [ForeignKey("ConsultantId")]
+        
+        public Consultant? Consultant { get; set; }
+        public string? RefNo { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal CashAmount { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
---
+++ HospitalMgrSystem.Service/ClaimBill/ClaimBillService.cs
@@ -0,0 +1,71 @@
+﻿using HospitalMgrSystem.DataAccess;
+using HospitalMgrSystem.Model;
+using HospitalMgrSystem.Service.ChannelingSchedule;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgrSystem.Service.ClaimBill
+{
+    public class ClaimBillService : IClaimBill
+    {
+        public Model.ClaimBill CreateClaimBill(Model.ClaimBill claimBill)
+        {
+            HospitalDBContext dBContext = new HospitalDBContext();
+
+            try
+            {
+                if (claimBill != null && claimBill.Id == 0)
+                {
+                    dBContext.ClaimBills.Add(claimBill);
+                    dBContext.SaveChanges();
+                } else
+                {
+                    Model.ClaimBill claimBillToUpdate = dBContext.ClaimBills.Where(x => x.Id == claimBill.Id).SingleOrDefault();
+
+                    claimBillToUpdate.PatientID = claimBill.PatientID;
+                    claimBillToUpdate.ConsultantId = claimBill.ConsultantId;
+                    claimBillToUpdate.RefNo = claimBill.RefNo;
+                    claimBillToUpdate.SubTotal = claimBill.SubTotal;
+                    claimBillToUpdate.Discount = claimBill.Discount;
+                    claimBillToUpdate.TotalAmount = claimBill.TotalAmount;
+                    claimBillToUpdate.CashAmount = claimBill.CashAmount;
+                    claimBillToUpdate.Balance = claimBill.Balance;
+                    claimBillToUpdate.CreateDate = claimBill.CreateDate;
+                    claimBillToUpdate.ModifiedDate = DateTime.Now;
+
+                    dBContext.SaveChanges();
+                }
+
+                return dBContext.ClaimBills
+                    .Include(x => x.Patient)
+                    .Include(x => x.Consultant)
+                    .Where(x => x.Id == claimBill.Id)
+                    .SingleOrDefault();
+                    
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Model.ClaimBill DeleteClaimBill(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.ClaimBill GetClaimBillById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Model.ClaimBill> GetClaimBillByPatientId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
