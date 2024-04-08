e4e25628faab396f5f8b5a145ee55f755905638e
ravindu0823 <guestpc87@gmail.com>
2024-04-08 17:34:01 +0530

    Updated the CashierSessionDto

--- HospitalMgrSystemUI/Models/CashierSessionDto.cs
+++ HospitalMgrSystemUI/Models/CashierSessionDto.cs
@@ -8,7 +8,9 @@ namespace HospitalMgrSystemUI.Models
         public List<CashierSession> CashierSessions { get; set; }
         public int cashierSessionID { get; set; }
         public CashierSession cashierSession { get; set; }
+        public CashierSession CashierPaymentData { get; set; }
         public User user { get; set; }
         public DateTime sessionDate { get; set; }
+        public int sessionId { get; set; }
     }
 }
