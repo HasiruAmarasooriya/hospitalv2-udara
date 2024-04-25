f11ea76e60d3ffac0607027d302492be7cb0c418
ravindu0823 <guestpc87@gmail.com>
2024-04-25 16:13:22 +0530

    Added the Rooms list to the ChannelingSheduleDto.cs

--- HospitalMgrSystemUI/Models/ChannelingSheduleDto.cs
+++ HospitalMgrSystemUI/Models/ChannelingSheduleDto.cs
@@ -8,6 +8,7 @@ namespace HospitalMgrSystemUI.Models
     {
         public List<Specialist> Specialists { get; set; }
         public List<Consultant> Consultants { get; set; }
+        public List<Room> Rooms { get; set; }
         public ChannelingSchedule ChannelingSchedule { get; set; }
         public List<ChannelingSchedule> ChannelingScheduleList { get; set; }
         public string ConsultantName { get; set; }
