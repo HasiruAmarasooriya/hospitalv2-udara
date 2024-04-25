7e6fbe3c3762e2e12db573ff367d8e9498da80f0
ravindu0823 <guestpc87@gmail.com>
2024-04-25 16:14:11 +0530

    Updated the RoomService.cs

--- HospitalMgrSystem.Service/Room/RoomService.cs
+++ HospitalMgrSystem.Service/Room/RoomService.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HospitalMgrSystem.DataAccess;
 
 namespace HospitalMgrSystem.Service.Room
 {
     public class RoomService : IRoomService
     {
-
         public List<Model.Room> GetRooms()
         {
-            List<Model.Room> objRoom = new List<Model.Room>();
+            var objRoom = new List<Model.Room>();
 
             try
             {
-                using (HospitalMgrSystem.DataAccess.HospitalDBContext dbContext = new HospitalMgrSystem.DataAccess.HospitalDBContext())
-                {
-
-                    objRoom = dbContext.Rooms.ToList();
-                }
+                using var dbContext = new HospitalDBContext();
+                objRoom = dbContext.Rooms.ToList();
             }
             catch (Exception ex)
-            { }
+            {
+                // ignored
+            }
+
             return objRoom;
         }
     }
