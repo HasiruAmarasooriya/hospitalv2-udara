39776ddb8230785e9b5cdd75acd80f9f9ec375cd
kalhararajapaksha <rmkkrajapaksha@students.nsbm.ac.lk>
2024-06-10 09:30:12 +0530

    Fix SMS Re Sending Issue

---
+++ HospitalMgrSystem.Model/Enums/ChannelingScheduleSMSStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgrSystem.Model.Enums
+{
+    public enum ChannelingScheduleSMSStatus
+    {
+        NOT_YET,
+        SESSION_START_SMS_SENT,
+        SESSION_END_SMS_SENT,
+        SESSION_PENDING_SMS_SENT,
+        SESSION_CANCEL_SMS_SENT,
+        SESSIOM_TIME_CHANGE_SMS_SENT
+    }
+}
