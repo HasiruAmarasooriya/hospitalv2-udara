10453a534fac2e31da33bff72643d990ed1f9dbc
ravindu0823 <guestpc87@gmail.com>
2024-06-11 18:31:03 +0530

    Created DTOs for SP

---
+++ HospitalMgrSystem.Model/DTO/ReportOpdXrayOtherRefundDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HospitalMgrSystem.Model.DTO
+{
+	[Keyless]
+	public class ReportOpdXrayOtherRefundDTO
+	{
+		public int InvoiceId { get; set; }
+		public int OpdId { get; set; }
+		public DateTime RefundedDate { get; set; }
+		public string Cashier { get; set; }
+		public string PatientName { get; set; }
+		public string Description { get; set; }
+		public decimal Amount { get; set; }
+	}
+}
