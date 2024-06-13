fef120c4e8143ce633d0a91edf6bc6a541b7879d
ravindu0823 <guestpc87@gmail.com>
2024-06-13 23:30:12 +0530

    Created the DTO for the OPD Other and X-RAY

---
+++ HospitalMgrSystem.Model/DTO/OpdOtherXrayDataTableDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HospitalMgrSystem.Model.DTO
+{
+    public class OpdOtherXrayDataTableDto
+    {
+        public int Id { get; set; }
+        public string roomName { get; set; }
+        public string consultantName { get; set; }
+        public string FullName { get; set; }
+        public string MobileNumber { get; set; }
+        public DateTime DateTime { get; set; }
+        public int Sex { get; set; }
+        public int Status { get; set; }
+        public int paymentStatus { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
