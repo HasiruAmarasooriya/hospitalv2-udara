7abd0f0c937a1564d191e9c60093a0f19ec049dd
ravindu0823 <guestpc87@gmail.com>
2024-06-23 21:32:15 +0530

    Created DTO for the Payment SUmmary for Doctors in OPD

---
+++ HospitalMgrSystem.Model/DTO/PaymentSummaryOfDoctorsOPDDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgrSystem.Model.DTO
+{
+	[Keyless]
+	public class PaymentSummaryOfDoctorsOPDDTO
+	{
+		public string ConsultantName { get; set; }
+		public decimal TotalAmount { get; set; }
+		public decimal TotalPaidAmount { get; set; }
+		public decimal TotalRefundAmount { get; set; }
+	}
+}
