0f0c26f16a58d83dc37992cf68081ccb9d904d2c
ravindu0823 <guestpc87@gmail.com>
2024-06-23 21:31:56 +0530

    Created the DTO for the Forward Booking Data Table

---
+++ HospitalMgrSystem.Model/DTO/ForwardBookingDataTableDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgrSystem.Model.DTO
+{
+	[Keyless]
+	public class ForwardBookingDataTableDTO
+	{
+		public int ScheduleId { get; set; }
+		public string DoctorName { get; set; }
+		public DateTime ScheduleDate { get; set; }
+		public decimal PaidAmount { get; set; }
+	}
+}
