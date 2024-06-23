c721cdda69854b86f9428f87f3960988c45379d9
ravindu0823 <guestpc87@gmail.com>
2024-06-23 21:33:31 +0530

    Refactored the Claim bill Model

--- HospitalMgrSystem.Model/ClaimBill.cs
+++ HospitalMgrSystem.Model/ClaimBill.cs
@@ -2,26 +2,25 @@
 
 namespace HospitalMgrSystem.Model
 {
-    public class ClaimBill
-    {
-        public int Id { get; set; }
-        public int PatientID { get; set; }
-
-        [ForeignKey("PatientID")]
-        public Patient? Patient { get; set; }
-        public int ConsultantId { get; set; }
-
-        [ForeignKey("ConsultantId")]
-        
-        public Consultant? Consultant { get; set; }
-        public string? RefNo { get; set; }
-        public decimal SubTotal { get; set; }
-        public decimal Discount { get; set; }
-        public decimal TotalAmount { get; set; }
-        public decimal CashAmount { get; set; }
-        public decimal Balance { get; set; }
-        public DateTime CreateDate { get; set; }
-        public DateTime ModifiedDate { get; set; }
-
-    }
+	public class ClaimBill
+	{
+		public int Id { get; set; }
+		public int PatientID { get; set; }
+		[ForeignKey("PatientID")] public Patient? Patient { get; set; }
+		public int ConsultantId { get; set; }
+		[ForeignKey("ConsultantId")] public Consultant? Consultant { get; set; }
+		public int? InvoiceId { get; set; }
+		[ForeignKey("InvoiceId")] public Invoice? Invoice { get; set; }
+		public string? RefNo { get; set; }
+		public decimal SubTotal { get; set; }
+		public decimal Discount { get; set; }
+		public decimal TotalAmount { get; set; }
+		public decimal CashAmount { get; set; }
+		public decimal Balance { get; set; }
+		public int Status { get; set; }
+		public int CsId { get; set; }
+		[ForeignKey("CsId")] public CashierSession? CashierSession { get; set; }
+		public DateTime CreateDate { get; set; }
+		public DateTime ModifiedDate { get; set; }
+	}
 }
