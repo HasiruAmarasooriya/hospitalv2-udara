25ae7359462c77f2027e5b8bcdda5a502a6ce743
ravindu0823 <guestpc87@gmail.com>
2024-07-23 22:44:59 +0530

    Created an DTO for the Cashier Session that coming from the SP

---
+++ HospitalMgrSystem.Model/DTO/CashierSessionDTO.cs
@@ -0,0 +1,19 @@
+﻿using HospitalMgrSystem.Model.Enums;
+
+namespace HospitalMgrSystem.Model.DTO
+{
+    public class CashierSessionDTO
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public int UserId { get; set; }
+        public CashierSessionStatus cashierSessionStatus { get; set; }
+        public DateTime StartingTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal StartBalence { get; set; }
+        public decimal EndBalence { get; set; }
+        public UserRole userRole { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal Variation { get; set; }
+    }
+}
