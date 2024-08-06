4e792eaadf240db8e330dfd3794b0bf462628ccc
ravindu0823 <guestpc87@gmail.com>
2024-08-06 14:09:10 +0530

    Created new DTOs for the Channeling Report

---
+++ HospitalMgrSystem.Model/DTO/PreviousForwardBookingDataDto.cs
@@ -0,0 +1,15 @@
+﻿namespace HospitalMgrSystem.Model.DTO
+{
+	public class PreviousForwardBookingDataDto
+	{
+		public int Id { get; set; }
+		public int OpdId { get; set; }
+		public int InvoiceId { get; set; }
+		public string FullName { get; set; }
+		public int AppoimentNo { get; set; }
+		public string DocName { get; set; }
+		public string DoctorPaidBy { get; set; }
+		public DateTime RegisteredDate { get; set; }
+		public decimal TotalPaidAmount { get; set; }
+	}
+}
