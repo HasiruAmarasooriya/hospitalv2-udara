3045949eb8681cada6d8f494f64d08aa21663151
ravindu0823 <guestpc87@gmail.com>
2024-09-08 15:04:52 +0530

    Added new functions

--- HospitalMgrSystem.Service/ClaimBill/ClaimBillService.cs
+++ HospitalMgrSystem.Service/ClaimBill/ClaimBillService.cs
@@ -4,90 +4,137 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HospitalMgrSystem.Service.ClaimBill
 {
-    public class ClaimBillService : IClaimBill
-    {
+	public class ClaimBillService : IClaimBill
+	{
 
-	    public List<ClaimBillDto> GetAllClaimBillsSP()
-	    {
-		    var mtList = new List<ClaimBillDto>();
-		    using (var context = new HospitalDBContext())
-		    {
-			    mtList = context.Set<ClaimBillDto>()
-				    .FromSqlRaw("EXEC GetAllClaimBillHistory")
-				    .ToList();
+		public List<ClaimBillDto> GetAllClaimBillsSP()
+		{
+			var mtList = new List<ClaimBillDto>();
+			using (var context = new HospitalDBContext())
+			{
+				mtList = context.Set<ClaimBillDto>()
+					.FromSqlRaw("EXEC GetAllClaimBillHistory")
+					.ToList();
 
-		    }
-		    return mtList;
+			}
+			return mtList;
 		}
 
-	    public ClaimBillDto? GetAllClaimBillsSP(int id)
-	    {
-		    ClaimBillDto? claimBill;
+		public ClaimBillDto? GetAllClaimBillsSP(int id)
+		{
+			ClaimBillDto? claimBill;
 
-		    using (var context = new HospitalDBContext())
-		    {
-			    claimBill = context.Set<ClaimBillDto>()
-				    .FromSqlRaw("EXEC GetAllClaimBillHistoryById @Id = {0}", id)
-				    .FirstOrDefault();
+			using (var context = new HospitalDBContext())
+			{
+				claimBill = context.Set<ClaimBillDto>()
+					.FromSqlRaw("EXEC GetAllClaimBillHistoryById @Id = {0}", id)
+					.FirstOrDefault();
 
-		    }
-		    return claimBill;
-	    }
+			}
+			return claimBill;
+		}
+
+		public List<Model.ClaimBillItems> GetChannelingItemsNames(List<Model.ClaimBillItems> billItemsList)
+		{
+			var dBContext = new HospitalDBContext();
+
+			try
+			{
+				foreach (var item in billItemsList)
+				{
+					item.ItemName = item.ItemType switch
+					{
+						"CHE" => dBContext.ChannelingItems.Where(x => x.Id == item.ScanItemId)
+							.Select(x => x.ItemName)
+							.SingleOrDefault(),
+						"OPD" => dBContext.Drugs.Where(x => x.Id == item.ScanItemId)
+							.Select(x => x.DrugName)
+							.SingleOrDefault(),
+						_ => item.ItemName
+					};
+				}
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
+			return billItemsList;
+		}
+
+
+		public List<Model.ClaimBillItems> CreateClaimBillItemsList(List<Model.ClaimBillItems> billItemsList)
+		{
+			var dBContext = new HospitalDBContext();
+
+			try
+			{
+				dBContext.ClaimBillItemsData.AddRange(billItemsList);
+				dBContext.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
+			return billItemsList;
+		}
 
 		public Model.ClaimBill CreateClaimBill(Model.ClaimBill claimBill)
-        {
-            var dBContext = new HospitalDBContext();
+		{
+			var dBContext = new HospitalDBContext();
 
-            try
-            {
-                if (claimBill != null && claimBill.Id == 0)
-                {
-                    dBContext.ClaimBills.Add(claimBill);
-                    dBContext.SaveChanges();
-                } else
-                {
-                    var claimBillToUpdate = dBContext.ClaimBills.Where(x => x.Id == claimBill.Id).SingleOrDefault();
+			try
+			{
+				if (claimBill != null && claimBill.Id == 0)
+				{
+					dBContext.ClaimBills.Add(claimBill);
+					dBContext.SaveChanges();
+				}
+				else
+				{
+					var claimBillToUpdate = dBContext.ClaimBills.Where(x => x.Id == claimBill.Id).SingleOrDefault();
 
-                    claimBillToUpdate.PatientID = claimBill.PatientID;
-                    claimBillToUpdate.ConsultantId = claimBill.ConsultantId;
-                    claimBillToUpdate.RefNo = claimBill.RefNo;
-                    claimBillToUpdate.SubTotal = claimBill.SubTotal;
-                    claimBillToUpdate.Discount = claimBill.Discount;
-                    claimBillToUpdate.TotalAmount = claimBill.TotalAmount;
-                    claimBillToUpdate.CashAmount = claimBill.CashAmount;
-                    claimBillToUpdate.Balance = claimBill.Balance;
-                    claimBillToUpdate.CreateDate = claimBill.CreateDate;
-                    claimBillToUpdate.ModifiedDate = DateTime.Now;
+					claimBillToUpdate.PatientID = claimBill.PatientID;
+					claimBillToUpdate.ConsultantId = claimBill.ConsultantId;
+					claimBillToUpdate.RefNo = claimBill.RefNo;
+					claimBillToUpdate.SubTotal = claimBill.SubTotal;
+					claimBillToUpdate.Discount = claimBill.Discount;
+					claimBillToUpdate.TotalAmount = claimBill.TotalAmount;
+					claimBillToUpdate.CashAmount = claimBill.CashAmount;
+					claimBillToUpdate.Balance = claimBill.Balance;
+					claimBillToUpdate.CreateDate = claimBill.CreateDate;
+					claimBillToUpdate.ModifiedDate = DateTime.Now;
 
-                    dBContext.SaveChanges();
-                }
+					dBContext.SaveChanges();
+				}
 
-                return dBContext.ClaimBills
-                    .Include(x => x.Patient)
-                    .Include(x => x.Consultant)
-                    .Where(x => x.Id == claimBill.Id)
-                    .SingleOrDefault();
-                    
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
+				return dBContext.ClaimBills
+					.Include(x => x.Patient)
+					.Include(x => x.Consultant)
+					.Where(x => x.Id == claimBill.Id)
+					.SingleOrDefault();
 
-        public Model.ClaimBill DeleteClaimBill(int id)
-        {
-            throw new NotImplementedException();
-        }
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
 
-        public Model.ClaimBill GetClaimBillById(int id)
-        {
-            throw new NotImplementedException();
-        }
+		public Model.ClaimBill DeleteClaimBill(int id)
+		{
+			throw new NotImplementedException();
+		}
 
-        public List<Model.ClaimBill> GetClaimBillByPatientId(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public Model.ClaimBill GetClaimBillById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Model.ClaimBill> GetClaimBillByPatientId(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
