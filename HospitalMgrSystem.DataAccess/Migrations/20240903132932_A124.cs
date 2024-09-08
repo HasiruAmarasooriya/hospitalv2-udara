6893d97bd8dda0ee4f52efa22e40919f1c68812c
ravindu0823 <guestpc87@gmail.com>
2024-09-08 15:12:52 +0530

    Added migrations to claim bill items data

---
+++ HospitalMgrSystem.DataAccess/Migrations/20240903132932_A124.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalMgrSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class A124 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItemType",
+                table: "ClaimBillItemsData",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemType",
+                table: "ClaimBillItemsData");
+        }
+    }
+}
---
+++ HospitalMgrSystem.DataAccess/Migrations/20240904093712_A125.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalMgrSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class A125 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "ClaimBillItemsData",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "ClaimBillItemsData",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Qty",
+                table: "ClaimBillItemsData",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "ClaimBillItemsData");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "ClaimBillItemsData");
+
+            migrationBuilder.DropColumn(
+                name: "Qty",
+                table: "ClaimBillItemsData");
+        }
+    }
+}
