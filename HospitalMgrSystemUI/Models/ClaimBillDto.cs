aff7bf9681e446f40580dc1f909cb6a40f4d0d0e
ravindu0823 <guestpc87@gmail.com>
2024-09-08 15:03:47 +0530

    Updated the claim bill DTO

--- HospitalMgrSystemUI/Models/ClaimBillDto.cs
+++ HospitalMgrSystemUI/Models/ClaimBillDto.cs
@@ -9,15 +9,21 @@ namespace HospitalMgrSystemUI.Models
         public List<HospitalMgrSystem.Model.DTO.ClaimBillDto>? claimBillDtos { get; set; }
 		public List<Patient>? patientsList { get; set; }
         public List<Consultant>? consultantsList { get; set; }
-        public DateTime dateTime { get; set; }
+        public List<Scan>? Scans { get; set; }
+        public List<Drug>? Drugs { get; set; }
+        public List<ClaimBillItems>? ClaimBillItemsList { get; set; }
+		public DateTime dateTime { get; set; }
         public string PatientName { get; set; }
         public string RefNo { get; set; }
         public string ContactNumber { get; set; }
-        public int ConsultantId { get; set; }
+        public string NIC { get; set; }
+		public int ConsultantId { get; set; }
         public decimal SubTotal { get; set; }
         public decimal Discount { get; set; }
         public decimal TotalAmount { get; set; }
         public decimal Cash { get; set; }
         public decimal Balance { get; set; }
+        public decimal HospitalFee { get; set; }
+        public decimal ConsultantFee { get; set; }
     }
 }
