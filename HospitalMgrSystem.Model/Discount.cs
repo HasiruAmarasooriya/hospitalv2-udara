0f45a7d75d3d359554949a4ee5817d85969da01e
Ravindu Perera <guestpc87@gmail.com>
2024-09-24 19:04:36 +0530

    Created a new model called Discount

---
+++ HospitalMgrSystem.Model/Discount.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalMgrSystem.Model;
+
+public class Discount
+{
+    public int Id { get; set; }
+    public decimal Percentage { get; set; }
+    public int Status { get; set; }
+    public int? CreateUser { get; set; }
+    [ForeignKey(nameof(CreateUser))] public User? CrUser { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public int? ModifiedUser { get; set; }
+    [ForeignKey(nameof(ModifiedUser))] public User? MdUser { get; set; }
+}
