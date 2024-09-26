6cb9fbdbba64a446664aeb5f68d960a7a7bf7dfd
Ravindu Perera <guestpc87@gmail.com>
2024-09-26 10:31:47 +0530

    Add ClaimBillCategoryType enum and necessary using directives
    Introduced a new enumeration `ClaimBillCategoryType` in the
    `HospitalMgrSystem.Model.Enums` namespace to categorize claim
    bills into `INVALID`, `OPD`, `XRAY`, `OTHER`, and `CHANNELING`.
    Added `using` directives for `System`, `System.Collections.Generic`,
    `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ HospitalMgrSystem.Model/Enums/ClaimBillCategoryType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMgrSystem.Model.Enums
+{
+	public enum ClaimBillCategoryType
+	{
+		INVALID,
+		OPD,
+		XRAY,
+		OTHER,
+		CHANNELING
+	}
+}
