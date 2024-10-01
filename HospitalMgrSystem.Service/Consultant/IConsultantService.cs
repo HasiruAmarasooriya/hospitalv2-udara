f453f9aecfed6e1e1d07630503e98c2e3570866e
Udara4 <lasithudara@gmail.com>
2024-10-01 14:45:31 +0530

    Channeling bill is refactored to print two copies: one for the customer and one for the office records.

--- HospitalMgrSystem.Service/Consultant/IConsultantService.cs
+++ HospitalMgrSystem.Service/Consultant/IConsultantService.cs
@@ -8,5 +8,7 @@
         public Model.Consultant GetAllConsultantByID(int? id);
         public Model.Consultant DeleteConsultant(Model.Consultant consultant);
         public List<Model.Consultant> SearchConsultant(string value);
-    }
+        public List<Model.Consultant> GetAllOPDConsultantByStatus();
+
+	}
 }
